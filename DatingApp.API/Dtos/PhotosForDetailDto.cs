5e358588a6288b7fcf05c647a28cfb939c1dbbc1
Lin <yi.lin_1@philips.com>
2020-09-27 15:19:57 -0700

    Extending API

---
+++ DatingApp.API/Dtos/PhotosForDetailDto.cs
@@ -0,0 +1,15 @@
+using System;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Dtos
+{
+    public class PhotosForDetailDto
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public DateTime DateAdded { get; set; }
+        public bool IsMain { get; set; }
+        
+    }
+}
